e66bde0807c658e59aa2e79f0d99edb38cda6916
Andrey Bruhanov <mail@alamra.net>
2020-09-12 12:57:30 +0200

    Вынес настройки в env файл. Улучшил работу с папками и хостами.

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
-BX_ENV_WORKDIR=/etc/nginx
-BX_ENV_TEMPLATE=bx.template
-BX_ENV_TARGETDIR=sites-enabled
+BX_WORKDIR=/etc/nginx
+BX_TEMPLATE=bx.template
+BX_TARGETDIR=sites-enabled
 WWW_DIR=/var/www/public_html
-DEFAULT=0
 
 # create nginx configs
-cd $WWW_DIR || exit
+cd ${WWW_DIR} || exit
 
-[[ ! (-d "${BX_ENV_WORKDIR}/${BX_ENV_TARGETDIR}") ]] && mkdir -p "${BX_ENV_WORKDIR}/${BX_ENV_TARGETDIR}" || exit
+[[ ! (-d "${BX_WORKDIR}/${BX_TARGETDIR}") ]] && mkdir -p "${BX_WORKDIR}/${BX_TARGETDIR}" || exit
 
 for f in *
 do
-  if [[ ! (-d $f) ]]
-  then
-    continue
-  fi
+  [[ ! (-d ${f}) ]] && continue
 
-  TEMPLATE="${BX_ENV_WORKDIR}/${BX_ENV_TEMPLATE}"
-  OUTPUT="${BX_ENV_WORKDIR}/${BX_ENV_TARGETDIR}/${f}.conf"
+  OUTPUT="${BX_WORKDIR}/${BX_TARGETDIR}/${f}.conf"
+  [[ -f ${OUTPUT} ]] && continue
 
-  DEFAULT_STRING=""
-  if [[ $DEFAULT == 0 ]]
-  then
-    DEFAULT_STRING="default_server"
-    DEFAULT=1
-  fi
+  TEMPLATE="${BX_WORKDIR}/${BX_TEMPLATE}"
 
-  [[ -f $f ]] && continue
+  [[ ${f} =~ "." ]] && HOST=${f} || HOST="${f}.${BX_DEFAULT_LOCAL_DOMAIN}"
+  [[ ${HOST} == ${BX_DEFAULT_HOST} ]] && DEFAULT=" default_server" || DEFAULT=""
 
-  touch "${OUTPUT}" && sed -e "s/%NAME%/${f}/; s/%DEFAULT%/${DEFAULT_STRING}/" "${TEMPLATE}" > ${OUTPUT}
+  touch "${OUTPUT}" && sed -e "s/%HOST%/${HOST}/; s/%NAME%/${f}/; s/%DEFAULT%/${DEFAULT}/" "${TEMPLATE}" > ${OUTPUT}
 done
 ########################
 
